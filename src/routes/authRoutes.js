7a8045e6359dff54595d41321d22dbbcabf66fa8
Thiago Augusto dos santos <96118290+ThiagoAugusto2017@users.noreply.github.com>
2022-04-03 15:06:01 -0300

    rota

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,5 +6,10 @@ const authController = require('../controller/authcontroller');
 router.get('/login', authController.login)
 router.get('/register', authController.register)
 router.post('/register', authController.registerpost) // metodo de registro de dados no banco
+router.get('/logout', authController.logout); //saida do sistema
+
+
+
+
 
 module.exports = router;
