ef05566b419cbee501551c26d7d7f2c0c256845d
thiagoAugusto2017 <thiago_augusto2017@hotmail.com>
2022-04-04 18:25:32 -0300

    proj04.04

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,7 @@ const authController = require('../controller/authcontroller');
 
 //*rotas
 router.get('/login', authController.login)
+router.post('/login', authController.loginpost);
 router.get('/register', authController.register)
 router.post('/register', authController.registerpost) // metodo de registro de dados no banco
 router.get('/logout', authController.logout); //saida do sistema
