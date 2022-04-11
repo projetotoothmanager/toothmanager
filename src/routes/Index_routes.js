e29e34599220b4b70c9977859311787ed1deacb0
Thiago Augusto dos santos <96118290+ThiagoAugusto2017@users.noreply.github.com>
2022-04-10 21:07:28 -0300

    correção

--- src/routes/Index_routes.js
+++ src/routes/Index_routes.js
@@ -7,48 +7,48 @@ router.get('/', Index_controller.show_Home)
 
 //Tem que ser alterado
 
-/* AGENDA */
-router.get('/agenda', function (req, res, next) {
+// /* AGENDA */
+// router.get('/agenda', function (req, res, next) {
 
-  res.render('agendamento', {
-    title: 'Express'
-  })
-});
-/* CADASTRO */
-router.get('/cadastro', function (req, res, next) {
+//   res.render('agendamento', {
+//     title: 'Express'
+//   })
+// });
+// /* CADASTRO */
+// router.get('/cadastro', function (req, res, next) {
 
-  res.render('cadastro', {
-    title: 'Express'
-  })
-});
-/* PRONTUARIO */
-router.get('/prontuario', function (req, res, next) {
+//   res.render('cadastro', {
+//     title: 'Express'
+//   })
+// });
+// /* PRONTUARIO */
+// router.get('/prontuario', function (req, res, next) {
 
-  res.render('listaProntuario', {
-    title: 'Express'
-  })
-});
+//   res.render('listaProntuario', {
+//     title: 'Express'
+//   })
+// });
 
-router.get('/criarprontuario', function (req, res, next) {
+// router.get('/criarprontuario', function (req, res, next) {
 
-  res.render('criarProntuario', {
-    title: 'Express'
-  })
-});
-/* PRONTUARIO DETALHADO DO PACIENTE */
-router.get('/prontuario/detalhes', function (req, res, next) {
+//   res.render('criarProntuario', {
+//     title: 'Express'
+//   })
+// });
+// /* PRONTUARIO DETALHADO DO PACIENTE */
+// router.get('/prontuario/detalhes', function (req, res, next) {
 
-  res.render('detalhesProntuario', {
-    title: 'Express'
-  })
-});
-/* PAGAMENTOS */
-router.get('/pagamentos', function (req, res, next) {
+//   res.render('detalhesProntuario', {
+//     title: 'Express'
+//   })
+// });
+// /* PAGAMENTOS */
+// router.get('/pagamentos', function (req, res, next) {
 
-  res.render('pagamentos', {
-    title: 'Express'
-  })
-});
+//   res.render('pagamentos', {
+//     title: 'Express'
+//   })
+// });
 
 
 module.exports = router;
