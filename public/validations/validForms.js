b810c970201443569e165218f7dc4aa7d1b45d78
WictorHiago <wictor.hsc@gmail.com>
2022-06-24 13:15:20 -0400

    validação de formulario login

---
+++ public/validations/validForms.js
@@ -0,0 +1,33 @@
+const button = document.getElementById('button')
+
+button.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  const email = document.getElementById('email')
+  const password = document.getElementById('password')
+
+  if(email.value =='') {
+    email.classList.add('errorInput')
+  }else{
+    email.classList.remove('errorInput')
+  }
+
+  if(password.value =='') {
+    password.classList.add('errorInput')
+  }else{
+    password.classList.remove('errorInput')
+  }
+
+  if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 || (email.value.indexOf('.') - email.value.indexOf('@') == 1)) {
+    email.classList.add('errorInput')
+  }else{
+    email.classList.remove('errorInput')
+  }
+
+  if(password.value.length <= 5) {
+    password.classList.add('errorInput')
+  }else{
+    password.classList.add('errorInput')
+  }
+
+})
