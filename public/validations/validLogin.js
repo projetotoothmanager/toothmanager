34be73582bc673ecb39e248d44e58ba2fc37a906
WictorHiago <wictor.hsc@gmail.com>
2022-06-26 15:34:26 -0400

    adicionando validações front-end

---
+++ public/validations/validLogin.js
@@ -0,0 +1,34 @@
+const button = document.getElementById('button')
+
+button.addEventListener('click', (event) => {
+  // event.preventDefault()
+
+  const email = document.getElementById('email')
+  const password = document.getElementById('password')
+
+  if(email.value =='') {
+    email.classList.add('errorInput')
+    event.preventDefault()
+  }else{
+    email.classList.remove('errorInput')
+  }
+
+  if(password.value =='') {
+    password.classList.add('errorInput')
+  }else{
+    password.classList.remove('errorInput')
+  }
+
+  if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 || (email.value.indexOf('.') - email.value.indexOf('@') == 1)) {
+    email.classList.add('errorInput')
+  }else{
+    email.classList.remove('errorInput')
+  }
+
+  if(password.value.length <= 4) {
+    console.log(password.classList.add('errorInput'))
+  }else{
+    password.classList.remove('errorInput')
+  }
+
+})
